9c714f8844bdba33b0eaa9b4e66072ba9f1a16f8
MatasA <aleliunasmatas@gmail.com>
2023-08-11 17:29:08 +0300

    chore: added submit btn value

--- pages/contactForm.tsx
+++ pages/contactForm.tsx
@@ -30,7 +30,7 @@ export default function ContactForm() {
               < textarea name='message' className={`${inter.className} ${styles.textArea}`} />
            </div>
            <div className={styles.submitBtnContainer}>
-            < input className={styles.submitBtn} type='submit'/>
+            < input className={styles.submitBtn} type='submit' value='Submit'/>
            </div>
               
   </form >
