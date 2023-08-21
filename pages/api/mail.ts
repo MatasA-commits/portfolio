8e28c71057fbfe8ef472df8eef6d17ecf48dab85
MatasA <aleliunasmatas@gmail.com>
2023-08-21 18:55:25 +0300

    chore: changed next grid api key

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
 type Data = {
   message: string;
