84e6435977ad169d26efc090928c7c633b3893fd
MatasA <aleliunasmatas@gmail.com>
2023-08-22 14:30:34 +0300

    chore: fixed whitespace bug in email input, diabled text area resize

--- components/contactForm/validate.ts
+++ components/contactForm/validate.ts
@@ -13,7 +13,7 @@ export const validate = ({
   }
   if (!email || email.trim() === "") {
     errors.email = "Email is required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email.trim())) {
     errors.email = "Invalid email address";
   }
   if (!message || message.trim() === "") {
