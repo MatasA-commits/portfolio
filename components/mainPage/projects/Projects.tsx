e3a6e19339da629269dd89cae813210dfececff1
MatasA <aleliunasmatas@gmail.com>
2023-08-28 20:22:26 +0300

    Chore: added more info about project

--- components/mainPage/projects/Projects.tsx
+++ components/mainPage/projects/Projects.tsx
@@ -7,6 +7,8 @@ const Projects = () => {
   return (
     <div className={styles.container}>
       <h3 className={indexStyles.infoTitle}>Projects</h3>
+      <p>Backend takes some time to spin up</p>
+      <p>You can use this login: username: Test12, password: Test@</p>
       <div className={styles.projectsContainer}>
         {ProjectsLinks.map(({ name, url, stack, state }) => (
           <ProjectLink key={url} name={name} url={url} stack={stack} state={state} />
